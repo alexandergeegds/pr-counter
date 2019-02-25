395bd0c9d255b917c850cb2e03aca06de0db364c
Alexander Gee <alexander.gee@digital.cabinet-office.gov.uk>
2019-02-25 15:37:34 +0000

    Add URL building logic
    Note that the GitHub API URLs may require authentication. But adding headers to requests using these URLS is currently beyond the scope of this module.

---
+++ url.js
@@ -0,0 +1,58 @@
+const search = {
+    prefix: 'https://github.com/search',
+    suffix: '&type=Issues',
+    replacements: {
+        ':': '%3A'
+    }
+};
+const api = {
+    prefix: 'https://api.github.com/search/issues'
+};
+
+function buildUrl(endpoint, options) {
+    let query = buildQuery(options);
+    if (endpoint.replacements) {
+        for (let replacement in endpoint.replacements) {
+            while (query.indexOf(replacement) > 0) {
+                query = query.replace(replacement, endpoint.replacements[replacement]);
+            }
+        }
+    }
+    return (endpoint.prefix ? endpoint.prefix : '') + '?q=' + query + (endpoint.suffix ? endpoint.suffix : '');
+}
+
+function buildQuery(options) {
+    query = [];
+    if (options.repo && !options.repos) {
+        options.repos = options.repo;
+    }
+    if (options.repos) {
+        if (typeof options.repos == 'string') {
+            options.repos = [options.repos]
+        }
+        for (let repo of options.repos) {
+            query.push('repo:' + repo);
+        }
+    }
+    query.push('is:pr');
+    query.push('is:merged');
+    if (options.start_date || options.end_date) {
+        dateQuery = ((options.start_date ? options.start_date : '*') + '..'
+                + (options.end_date ? options.end_date : '*'));
+        query.push('merged:' + dateQuery);
+    }
+    if (options.author && !options.authors) {
+        options.authors = options.author;
+    }
+    if (options.authors) {
+        if (typeof options.authors == 'string') {
+            options.authors = [options.authors]
+        }
+        for (let author of options.authors) {
+            query.push('author:' + author);
+        }
+    }
+    return query.join('+');
+}
+
+console.log(buildUrl(search, options));
